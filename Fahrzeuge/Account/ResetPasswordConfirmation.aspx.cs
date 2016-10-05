f9640a86ee2139634689faa024ef0bfd5109c798
Antonius Darma Setiawan <a.darma.setiawan@gmail.com>
2016-10-05 10:58:07 +0700

    Initial commit

---
+++ Fahrzeuge/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Fahrzeuge.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Fahrzeuge/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fahrzeuge.Startup))]
+namespace Fahrzeuge
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
